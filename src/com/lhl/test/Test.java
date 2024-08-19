43960df0f7cd2e874ce057179c9b1451637cf81f
WIFI连接超时 <localaccount@qq.com>
2024-08-20 02:08:49 +0800

    update doc

---
+++ src/com/lhl/test/Test.java
@@ -0,0 +1,25 @@
+package com.lhl.test;
+
+import com.lhl.bconsole.BetterConsole;
+import com.lhl.bconsole.CompText;
+
+/**
+ * @author WIFI连接超时
+ * @version 1.0
+ * Create Time: 2024/8/20_0:57
+ */
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        // 准备好你要显示什么
+        CompText compText = new CompText("Hello BConsole!");
+        // 告诉我你要显示什么
+        BetterConsole screen = BetterConsole.getScreen();
+        screen.reg(compText);
+        // 启动输出
+        screen.turnON();
+        // 模拟你自己的代码逻辑... 浅睡5秒吧
+        Thread.sleep(5000);
+        // 关闭输出
+        screen.turnOFF();
+    }
+}

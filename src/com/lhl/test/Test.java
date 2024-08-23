ac9bd6084acfd093f8c853d46d6ca49186aaef2e
WIFI连接超时 <localaccount@qq.com>
2024-08-23 17:31:05 +0800

    update example doc-2.9

--- src/com/lhl/test/Test.java
+++ src/com/lhl/test/Test.java
@@ -9,17 +9,57 @@ import com.lhl.bconsole.CompText;
  * Create Time: 2024/8/20_0:57
  */
 public class Test {
+    static String name = "张老三";
+    static int age = 8;
+    static int game = 0;
+    static int reallyGame = 0;
+    static boolean allowPlayGames = true;
+    static String msg = "你不管我我就继续玩！";
+
     public static void main(String[] args) throws InterruptedException {
-        // 准备好你要显示什么
-        CompText compText = new CompText("Hello BConsole!");
-        // 告诉我你要显示什么
+        // 拿到屏幕控制器
         BConsole screen = BConsole.getScreen();
-        screen.reg(compText);
-        // 启动输出
+        // 使用 CompText 存储带变量的文本内容
+        CompText text = new CompText("我叫$v$, 今年$v$岁了！\n我今天已经玩儿了$v$把王者了！\n$v$");
+        // 绑定变量
+        text.ref(v -> v.bind(name, age, game, msg));
+        // 注册文本内容到屏幕
+        screen.reg(text);
+        // 把显示期间的所有 System.out.println 存下来
+        screen.saveSystemOut("game-log.txt");
+        // 启用控制台显示屏
         screen.turnON();
-        // 模拟你自己的代码逻辑... 浅睡5秒吧
-        Thread.sleep(5000);
-        // 关闭输出
+        // 等待3秒后
+        Thread.sleep(3 * 1000);
+        // 张老三委托全局前置守卫来让你每次看他的游戏情况都是0
+        screen.beforeEach(() -> {
+            reallyGame = game;
+            game = 0;
+        });
+        // 张老三统计自己输赢的工作还得继续，于是委托全局后置守卫在你不看他的时候把游戏情况变回去
+        screen.afterEach(() -> {
+            game = reallyGame;
+        });
+        // 张老三去打王者了
+        new Thread(() -> {
+            // 张老三每200毫秒就能玩儿一把！
+            while (allowPlayGames) {
+                game++;
+                // 把这把游戏的情况打出来
+                System.out.println("今天的第" + game + "把游戏，" + (Math.random() < 0.2 ? "赢了！" : "输了..."));
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException ignore) {
+                }
+            }
+        }).start();
+        // 等待用户中断（继续）：waitUserInterrupt();
+        BConsole.getScreen().waitUserInterrupt();
+        // 张老三，快停下！
+        allowPlayGames = false;
+        // 再等待3秒后
+        Thread.sleep(3 * 1000);
+        // 关闭控制台显示屏，退出程序
         screen.turnOFF();
     }
 }

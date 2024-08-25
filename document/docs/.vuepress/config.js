d116027a6b86b79ec91d4f2640ae2a9e97bcec0c
WIFI连接超时 <localaccount@qq.com>
2024-08-25 15:10:21 +0800

    Revise new column

--- document/docs/.vuepress/config.js
+++ document/docs/.vuepress/config.js
@@ -9,32 +9,25 @@ module.exports = {
     logo: '/logo/logo.png',
     nav: [
       {text: '指南', link: '/guide/'},
-      {text: '进阶', link: ''},
+      {text: '示例项目', link: '/example/'},
       {text: 'Q&A', link: '/qa/'},
       {text: 'GitHub', link: 'https://github.com/wifi504/better-console'},
     ],
-    sidebar: [
-      {
-        title: '开 始',
-        path: '/guide/',
-      },
-      {
-        title: '基 础',
-        path: '/guide/base',
-      },
-      {
-        title: '深入组件',
-        path: '/guide/components',
-      },
-      {
-        title: '路 由',
-        path: '/guide/routers',
-      },
-      {
-        title: '渲染机制',
-        path: '/guide/renders',
-      }
-    ],
+    sidebar: {
+      '/guide/': [
+        {title: '开 始', path: '/guide/',},
+        {title: '基 础', path: '/guide/base',},
+        {title: '深入组件', path: '/guide/components',},
+        {title: '路 由', path: '/guide/routers',},
+        {title: '渲染机制', path: '/guide/renders',}
+      ],
+      '/example/': [
+        {title: '示例项目', path: '/example/'}
+      ],
+      '/qa/': [
+        {title: 'Q & A', path: '/qa/'}
+      ],
+    },
     smoothScroll: true,
     sidebarDepth: 1,
     lastUpdated: '最后更新时间'
---
+++ document/docs/qa/README.md
@@ -0,0 +1,3 @@
+# Q & A
+
+这里目前并没有什么内容，快来 [提问 (Issues)](https://github.com/wifi504/better-console/issues) 吧！

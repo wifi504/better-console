cb27ff6900cdff6470bfa9d065556395c81a77cb
WIFI连接超时 <localaccount@qq.com>
2024-12-05 02:43:39 +0800

    update PresetViewsFactory

---
+++ src/com/lhl/bconsole2/component/preset/ClockView.java
@@ -0,0 +1,38 @@
+package com.lhl.bconsole2.component.preset;
+
+import com.lhl.bconsole2.component.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 屏幕时钟
+ *
+ * @author WIFI连接超时
+ * @version 1.0
+ * Create Time 2024/12/5_1:52
+ */
+class ClockView extends View {
+
+    static Date date; // 每一刻的日期时间，伴随着组件被更新而重复实例化
+
+    // 构造传参标题
+    protected ClockView(Component<?> title) {
+        // 时间格式化器
+        SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
+        // 日期格式化器
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy年M月d日 EEEE");
+        // 艺术字动态时间
+        ArtText timeText = cf.newArtText("$v$").ref(v -> v.bind(sdfTime.format(date)));
+        // 动态日期
+        Text dateText = cf.newDivText("$v$").ref(v -> v.bind(sdfDate.format(date)));
+        // 钟的 Block 链式调用配置样式
+        Block block = cf.newBoldBlock(" \n$v$\n \n$v$\n \n$v$")
+                .ref(v -> v.bind(title, timeText, dateText)) // 设置标题、时间、日期
+                .setAlignment(Block.CENTER) // 设置居中对齐
+                .setWidth(80) // 块宽度 80 字符
+                .setHeight(12) // 块高度 12 行
+                .onBeforeRender(() -> date = new Date()); // 生命周期回调，刷新前获取最新时间
+        reg(block);
+    }
+}
---
+++ src/com/lhl/bconsole2/component/preset/PresetViewsFactory.java
@@ -0,0 +1,53 @@
+package com.lhl.bconsole2.component.preset;
+
+import com.lhl.bconsole2.callback.*;
+import com.lhl.bconsole2.component.*;
+
+/**
+ * 预设视图组件工厂
+ * <hr />
+ * 可以从这里获取 better-console 框架预先设计好的视图组件
+ *
+ * @author WIFI连接超时
+ * @version 1.0
+ * Create Time 2024/12/5_1:50
+ */
+public class PresetViewsFactory {
+
+    private static final ComponentFactory cf = new ComponentFactory();
+
+    // ---------------- 屏幕时钟 ---------------- //
+
+    /**
+     * 实例化一个屏幕时钟视图组件 <br />
+     * 使用默认标题 “屏 幕 时 钟”
+     *
+     * @return View
+     */
+    public View newClockView() {
+        return newClockView(v -> v.bind("屏 幕 时 钟"));
+    }
+
+    /**
+     * 实例化一个屏幕时钟视图组件 <br />
+     * 可以传入一个字符串作为静态标题
+     *
+     * @param title 标题
+     * @return View
+     */
+    public View newClockView(String title) {
+        return newClockView(v -> v.bind(title));
+    }
+
+    /**
+     * 实例化一个屏幕时钟视图组件 <br />
+     * 可以传入一个绑定器去绑定动态标题
+     *
+     * @param titleCallback 绑定器 v -> v.bind(值)
+     * @return View
+     */
+    public View newClockView(ObjectCallback titleCallback) {
+        return new ClockView(cf.newSpanText("$v$").ref(titleCallback));
+    }
+
+}
